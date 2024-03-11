59707367dc5a7cb94cbff7614d3563569ad1fd53
Husnain Zahid <husnainzahid214@gmail.com>
2024-03-11 17:36:05 +0500

    ""

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ mongoose
 
 app.set('view engine', 'ejs');
 app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
