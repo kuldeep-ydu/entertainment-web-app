6c4cf9101f7e3c4e7d6aceb5cdca41174c2cbbe3
Husnain Zahid <husnainzahid214@gmail.com>
2024-03-24 17:37:11 +0500

    ""

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -7,8 +7,12 @@ export default function AuthLayout() {
   const { user } = useContext(UserContext);
 
   return !user ? (
-    <div className="grid content-start justify-items-center py-1 pt-8">
-      <img src={logo} alt="app logo" className="mb-16 mt-8 block max-w-full" />
+    <div className="grid place-items-center p-3">
+      <img
+        src={logo}
+        alt="app logo"
+        className="absolute top-12 block max-w-full"
+      />
       <Outlet />
     </div>
   ) : (
