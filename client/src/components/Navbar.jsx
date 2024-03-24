bdf4f0a10104de4e273e092ef6f65fe5c31dcf59
Husnain Zahid <husnainzahid214@gmail.com>
2024-03-24 21:24:10 +0500

    "added more margin to navbar"

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import UserMenu from './UserMenu';
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 mt-0 flex items-center justify-between bg-secondary-dark px-6 py-4 sm:top-6 sm:mx-6 sm:mt-6 sm:rounded-lg md:h-[90vh] md:flex-col md:rounded-3xl md:py-8">
+    <nav className="sticky top-0 z-50 mt-0 flex items-center justify-between bg-secondary-dark px-6 py-4 sm:top-8 sm:mx-6 sm:mt-6 sm:rounded-lg md:h-[90vh] md:flex-col md:rounded-3xl md:py-8">
       <img src={Logo} alt="Entertainment Logo" height="32" width="32" />
 
       <ul className="grid grid-flow-col justify-items-center gap-4 sm:gap-6 md:mt-16 md:grid-flow-row">
