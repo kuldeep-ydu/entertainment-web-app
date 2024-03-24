f7eda8bb0eab16f14be9ce7dbe4ff4c8683fa724
Husnain Zahid <husnainzahid214@gmail.com>
2024-03-24 23:49:08 +0500

    ""

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -7,7 +7,7 @@ export default function AuthLayout() {
   const { user } = useContext(UserContext);
 
   return !user ? (
-    <div className="grid place-items-center content-center gap-8 sm:gap-16 p-3">
+    <div className="grid place-items-center content-center gap-16 p-3">
       <img src={logo} alt="app logo" className="block max-w-full" />
       <Outlet />
     </div>
